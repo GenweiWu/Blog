6a917bf9e449c914473e2f7aa940af96d662a52a
Dave.Wu <wugenwei2010@163.com>
2018-07-10 19:17:03 +0800

    Update Project.java

--- java/json/jacksonWriteValue/Project.java
+++ java/json/jacksonWriteValue/Project.java
@@ -7,8 +7,11 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+@Getter
 @Setter
+@ToString
 public class Project
 {
     @Getter
