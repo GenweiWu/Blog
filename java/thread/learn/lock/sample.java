8dc5eb8c8821f396a65bbb99cc634f1dbb916905
Dave.Wu <wugenwei2010@163.com>
2019-10-22 19:41:19 +0800

    Update sample.java

--- java/thread/learn/lock/sample.java
+++ java/thread/learn/lock/sample.java
@@ -1,3 +1,5 @@
+//参见 ReentrantReadWriteLock 的javadoc
+
 class CachedData {
    Object data;
    volatile boolean cacheValid;
@@ -7,6 +9,7 @@ class CachedData {
      rwl.readLock().lock();
      if (!cacheValid) {
        // Must release read lock before acquiring write lock
+        //读锁->写锁： 获取写锁之前，必须先释放读锁
        rwl.readLock().unlock();
        rwl.writeLock().lock();
        try {
@@ -17,6 +20,7 @@ class CachedData {
            cacheValid = true;
          }
          // Downgrade by acquiring read lock before releasing write lock
+         //写锁->读锁： 写锁可以降级为读锁，所以先获取读锁这个操作可以成功，然后再释放写锁 
          rwl.readLock().lock();
        } finally {
          rwl.writeLock().unlock(); // Unlock write, still hold read
