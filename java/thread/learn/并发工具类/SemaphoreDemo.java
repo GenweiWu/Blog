58675d369890c49ae9ba6a8c354e7cf9cb9cf19d
Dave.Wu <wugenwei2010@163.com>
2020-09-07 20:09:42 +0800

    Update SemaphoreDemo.java

--- java/thread/learn/并发工具类/SemaphoreDemo.java
+++ java/thread/learn/并发工具类/SemaphoreDemo.java
@@ -4,15 +4,34 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
+/**
+ * <pre>
+ * pool-1-thread-1 已进入: <---
+ * pool-1-thread-2 已进入: <---
+ * pool-1-thread-3 已进入: <---
+ * pool-1-thread-2 已退出: --->
+ * 当前可用个数(接近的):1
+ * pool-1-thread-5 已进入: <---
+ * pool-1-thread-3 已退出: --->
+ * pool-1-thread-4 已进入: <---
+ * 当前可用个数(接近的):0
+ * pool-1-thread-1 已退出: --->
+ * 当前可用个数(接近的):1
+ * pool-1-thread-5 已退出: --->
+ * 当前可用个数(接近的):2
+ * pool-1-thread-4 已退出: --->
+ * 当前可用个数(接近的):3
+ * </pre>
+ */
 public class SemaphoreDemo
 {
     public static void main(String[] args)
     {
         ExecutorService executorService = Executors.newCachedThreadPool();
         
-        Semaphore semaphore = new Semaphore(5);
+        Semaphore semaphore = new Semaphore(3);
         
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 5; i++)
         {
             executorService.execute(() -> {
                 
