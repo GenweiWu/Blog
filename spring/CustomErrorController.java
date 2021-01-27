fda699287b81291ed57b661ca01641861aedf164
Dave.Wu <wugenwei2010@163.com>
2021-01-27 11:28:20 +0800

    Create CustomErrorController.java

---
+++ spring/CustomErrorController.java
@@ -0,0 +1,15 @@
+@RestController
+public class CustomErrorController implements ErrorController{
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "Error handling";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
