7a8113676f0f66d4e7f91ea7432c0b14eeb67461
Dave.Wu <wugenwei2010@163.com>
2023-01-18 15:09:19 +0800

    Create set1.py

---
+++ python/learn/set1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+def set_create():
+	s = set([])
+	print(s)
+
+	s = set([1,2])
+	print(s)
+
+	# -- dict也是类似的{}
+	s= {3,4}
+	print(s)
+
+	# --- 即{}认为是dict而不是set
+	dict1 = {}
+	print(type(dict1)) # <class 'dict'>
+
+	# ---set中元素是不重复的
+	s = set([1,2,3,2,1])
+	print(s)
+	pass
+
+
+def set_add():
+	# --- add
+	s = set()
+	s.add('ABC')
+	print(s)
+	pass
+
+
+def set_delete():
+	# --- remove(key)
+	s = set([1,2,'ABC'])
+	s.remove('ABC')
+	print(s)
+	pass
+
+
+def set_read():
+	# --- 无法通过s['key']的方式来读取
+	s = set([1,2,'ABC'])
+	# TypeError: 'set' object is not subscriptable
+	# print(s['ABC'])
+	pass
+
+
+if __name__ == '__main__':
+	# set_create()
+	# set_add()
+	# set_delete()
+	set_read()
+	
