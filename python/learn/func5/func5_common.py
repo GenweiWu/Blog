489b9656d1517e01c3d4c27f08f6cc84ad8de8fb
Dave.Wu <wugenwei2010@163.com>
2023-01-31 18:34:09 +0800

    Create func5_common.py

---
+++ python/learn/func5/func5_common.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+def hello(name='default'):
+	print(f'Hello: {name}')
+	pass
+
+def bye(name='default'):
+	print(f'ByeBye: {name}')
+	pass
+
+# 引入模块
+# 引入模块 as 别名
+
+# 引入函数
+# 引入函数 as 别名
+# 引入所有函数 
