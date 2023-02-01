e27fdf119e3ce7dce4ebf68c6e99ff1971b76644
Dave.Wu <wugenwei2010@163.com>
2023-02-01 10:59:12 +0800

    Create many_car.py

---
+++ python/learn/class3/many_car.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+from car import Car,ElastricCar
+
+
+if __name__ == '__main__':
+	my_car = Car('audi','a4',2019)	
+	print(my_car.get_descriptive_name())
+
+	my_tesla = ElastricCar('tesla','model s',2023)
+	print(my_tesla.get_descriptive_name())
+	print(f'range: {my_tesla.battery.get_range()}')
