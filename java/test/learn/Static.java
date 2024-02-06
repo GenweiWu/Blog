e0e23a7030562d87f606ab7012bf5ad0491aa5dc
Dave.Wu <wugenwei2010@163.com>
2024-02-06 09:34:45 +0800

    Create Static.java

---
+++ java/test/learn/Static.java
@@ -0,0 +1,11 @@
+package com.njust.learn;
+
+public class Static {
+
+    public static String add(String a, String b) {
+        int result = Integer.parseInt(a) + Integer.parseInt(b);
+        System.out.println("result:" + result);
+        return String.valueOf(result);
+    }
+
+}
