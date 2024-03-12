c0500a2fe9b2d5bf1094bfac328a0027bccaa0ab
Dave.Wu <wugenwei2010@163.com>
2024-03-12 16:40:28 +0800

    Create BypassTool.java

---
+++ java/test/learn/BypassTool.java
@@ -0,0 +1,56 @@
+package com.njust.learn;
+
+/**
+ * 绕过封装
+ * <p>
+ * 1. 获取非公共属性
+ * <p>
+ * 2. 设置非公共属性
+ * <p>
+ * 3. 调用非公共方法
+ * <p>
+ * 4. 调用非公共构造函数
+ *
+ * <pre>{@code
+ * Use Whitebox.setInternalState(..) to set a non-public member of an instance or class.
+ * Use Whitebox.getInternalState(..) to get a non-public member of an instance or class.
+ * Use Whitebox.invokeMethod(..) to invoke a non-public method of an instance or class.
+ * Use Whitebox.invokeConstructor(..) to create an instance of a class with a private constructor.
+ * }
+ * </pre>
+ */
+public class BypassTool {
+
+    /**
+     * 私有属性
+     */
+    private int count = 0;
+
+    /**
+     * 私有构造函数
+     */
+    private BypassTool(int count) {
+        this.count = count;
+    }
+
+    public BypassTool() {
+        this(0);
+    }
+
+    public void increase() {
+        increaseCount(1);
+    }
+
+    /**
+     * 私有方法
+     */
+    private void increaseCount(int increase) {
+        count += increase;
+    }
+
+    public boolean checkCount(int target) {
+        return this.count == target;
+    }
+
+
+}
