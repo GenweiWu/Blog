ff2c67eee26d666d490e2aec86415cae4a384493
Dave.Wu <wugenwei2010@163.com>
2024-10-14 16:01:35 +0800

    Create LoadingCacheGetTest.java

---
+++ java/guava/LoadingCacheGetTest.java
@@ -0,0 +1,98 @@
+package com.njust.guaua;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.util.concurrent.UncheckedExecutionException;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * 测试不同的get方法
+ */
+@Slf4j
+public class LoadingCacheGetTest {
+
+    private LoadingCache<String, String> loadingCache;
+
+    @Before
+    public void init() {
+        this.loadingCache = CacheBuilder.newBuilder().build(new CacheLoader<String, String>() {
+            @Override
+            public String load(String key) throws Exception {
+                if (key.equals("error")) {
+                    throw new IOException("invalid key");
+                }
+                return key.toUpperCase(Locale.ENGLISH);
+            }
+        });
+    }
+
+    /**
+     * 1.直接调用get方法，会抛出checked异常:
+     * <p>
+     * 我们要么catch要么抛出
+     */
+    @Test
+    public void testGet() {
+        try {
+            loadingCache.get("error");
+        } catch (ExecutionException e) {
+            Throwable cause = e.getCause();
+            log.error("get value failed", cause);
+        }
+    }
+
+    /**
+     * 2.getUnchecked方法对get方法进行封装，改为抛出运行时异常
+     * <p>
+     * 参考源码:
+     * <pre>
+     *   {@code
+     *   public V getUnchecked(K key) {
+     *       try {
+     *         return get(key);
+     *       } catch (ExecutionException e) {
+     *         throw new UncheckedExecutionException(e.getCause());
+     *       }
+     *     }
+     *   }
+     * </pre>
+     */
+    @Test(expected = UncheckedExecutionException.class)
+    public void testGet2() {
+        loadingCache.getUnchecked("error");
+    }
+
+    /**
+     * 3.<pre>{@code V get(K key, Callable<? extends V> loader)}</pre>方法
+     * 实际上使用自定义的方法，来代替默认的load方法
+     * <p>
+     * 但是不推荐用这个方法，建议 `use LoadingCache and its get(K) method instead of this one.`
+     */
+    @Test
+    public void testGet3() throws ExecutionException {
+        //[1] using default loader
+        String key = "aaa";
+        String value = loadingCache.get(key);
+        Assert.assertEquals("AAA", value);
+
+        //[2] using custom loader
+        key = "bbb";
+        value = loadingCache.get(key, new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return "bbbNew_fromCall";
+            }
+        });
+        Assert.assertEquals("bbbNew_fromCall", value);
+    }
+
+}
